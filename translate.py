c61a03242f1564d3463f5aa85680bb9939e0e731
Jesse Karmani <jessekarmani@gmail.com>
2024-01-13 15:02:22 -0800

    Move to huggingface inference API and m2m100 model
    Temporarily using an open spaces API since there is no way
    to set target language on the default inference API

--- translate.py
+++ translate.py
@@ -1,53 +1,29 @@
 #!/usr/bin/env python
 
 import argparse
+import os
+import requests
+
+# use temporary spaces API to test for now (will be replaced by a permanent API)
+API_URL = "https://qgyd2021-multilingual-translation.hf.space/run/predict"
+
+def query(payload):
+    headers = {"Authorization": f"Bearer {os.environ['HUGGINGFACE_TOKEN']}"}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 def translate_text(target: str, text: str, source: str) -> dict:
-    """Translates text into the target language using Google Cloud Translate
-    API. Falls back to Helsinki-NLP LLM models if that fails.
-
-    Target and source must be an ISO 639-1 language code.
-    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
-
-    Returns a dictionary with the result, containing:
-      input: the input text
-      translatedText: the translated output text
-      detectedSourceLanguage: the detected language of the input text
+    """Translates text into the target language using the M2M100 model
     """
-    from google.cloud import translate_v2 as translate
-    from transformers import pipeline
-    import logging
-
-    logger = logging.getLogger(__name__)
-
-    translate_client = translate.Client()
-
-    if isinstance(text, bytes):
-        text = text.decode("utf-8")
-
-    try:
-        # Text can also be a sequence of strings, in which case this method
-        # will return a sequence of results for each text.
-        result = translate_client.translate(text, target_language=target)
-
-        print(u"Result: {}".format(result["translatedText"]))
-        return result
-    except Exception as e:
-        logger.exception(e)
-        print("Google Translate failed, falling back to Helsinki-NLP LLM models")
-
-        source_to_target_code = source + "-" + target
-        model_name = "Helsinki-NLP/opus-mt-" + source_to_target_code
-        translator = pipeline("translation", model=model_name)
-
-        result = translator(text)
-        print(u"Result: {}".format(result[0]["translation_text"]))
-        return result
+    payload = { "data": [text, source, target, "facebook/m2m100_418M"] }
+    result = query(payload)
+    print(result)
+    return result
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Translate text")
     parser.add_argument("target", help="target language")
     parser.add_argument("text", help="text to translate")
-    parser.add_argument("--source", required=False, help="source language")
+    parser.add_argument("source", help="source language")
     args = parser.parse_args()
     translate_text(args.target, args.text, args.source)
