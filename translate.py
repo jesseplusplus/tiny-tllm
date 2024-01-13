26ffc5c96af00a235845a174ec6f67bdbb9f57a4
Jesse Karmani <jessekarmani@gmail.com>
2024-01-12 18:22:23 -0800

    Add __main__ to run as script

--- translate.py
+++ translate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-def translate_text(target: str, text: str, source=None: str) -> dict:
+import argparse
+
+def translate_text(target: str, text: str, source: str) -> dict:
     """Translates text into the target language using Google Cloud Translate
     API. Falls back to Helsinki-NLP LLM models if that fails.
 
@@ -23,18 +25,29 @@ def translate_text(target: str, text: str, source=None: str) -> dict:
     if isinstance(text, bytes):
         text = text.decode("utf-8")
 
-    try
+    try:
         # Text can also be a sequence of strings, in which case this method
-    	# will return a sequence of results for each text.
-    	result = translate_client.translate(text, target_language=target)
+        # will return a sequence of results for each text.
+        result = translate_client.translate(text, target_language=target)
 
+        print(u"Result: {}".format(result["translatedText"]))
         return result
     except Exception as e:
-	logger.exception()
+        logger.exception(e)
+        print("Google Translate failed, falling back to Helsinki-NLP LLM models")
 
-	source_to_target_code = source + "-" + target
-	model_name = "Helsinki-NLP/opus-mt-" + source_to_target_code
-	translator = pipeline("translation", model=model_name)
+        source_to_target_code = source + "-" + target
+        model_name = "Helsinki-NLP/opus-mt-" + source_to_target_code
+        translator = pipeline("translation", model=model_name)
 
-	return translator(text)
+        result = translator(text)
+        print(u"Result: {}".format(result[0]["translation_text"]))
+        return result
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Translate text")
+    parser.add_argument("target", help="target language")
+    parser.add_argument("text", help="text to translate")
+    parser.add_argument("--source", required=False, help="source language")
+    args = parser.parse_args()
+    translate_text(args.target, args.text, args.source)
